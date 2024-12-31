2273298a54fd911521969bf8ad8b8d9710a241f8
Manpreet Kunnath <manpreetkunnath@gmail.com>
2024-12-31 17:53:03 +0530

    Initial Commit

---
+++ composeApp/src/commonMain/kotlin/fyi/manpreet/flowdiary/Greeting.kt
@@ -0,0 +1,9 @@
+package fyi.manpreet.flowdiary
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/fyi/manpreet/flowdiary/MainViewController.kt
@@ -0,0 +1,5 @@
+package fyi.manpreet.flowdiary
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
