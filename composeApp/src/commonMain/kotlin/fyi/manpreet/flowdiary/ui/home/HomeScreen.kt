b8a579b2c6e948358986a43a3505f77841a0b444
Manpreet Kunnath <manpreetkunnath@gmail.com>
2025-01-01 14:47:57 +0530

    Add HomeScreen

--- composeApp/src/commonMain/kotlin/fyi/manpreet/flowdiary/ui/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/fyi/manpreet/flowdiary/ui/home/HomeScreen.kt
@@ -1,11 +1,40 @@
 package fyi.manpreet.flowdiary.ui.home
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import fyi.manpreet.flowdiary.ui.home.components.appbar.HomeTopAppBar
+import fyi.manpreet.flowdiary.ui.home.components.empty.HomeScreenEmpty
+import fyi.manpreet.flowdiary.ui.theme.gradient
 
 @Composable
 fun HomeScreen(
-    modifier: Modifier = Modifier,
+    navController: NavController,
 ) {
 
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = { HomeTopAppBar() },
+        floatingActionButton = {},
+    ) { innerPadding ->
+        if (true) {
+            HomeScreenEmpty(
+                modifier = Modifier.padding(innerPadding).background(brush = MaterialTheme.gradient.background)
+            )
+            return@Scaffold
+        }
+
+        Box(
+            modifier = Modifier.fillMaxSize().padding(innerPadding)
+                .background(brush = MaterialTheme.gradient.background)
+        ) {
+
+        }
+    }
 }
