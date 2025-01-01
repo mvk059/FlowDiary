e3364d10762331a31bf4ff7b866c707dcd34fdb8
Manpreet Kunnath <manpreetkunnath@gmail.com>
2025-01-01 14:46:55 +0530

    Setup Home Navigation

--- composeApp/src/commonMain/kotlin/fyi/manpreet/flowdiary/App.kt
+++ composeApp/src/commonMain/kotlin/fyi/manpreet/flowdiary/App.kt
@@ -1,24 +1,15 @@
 package fyi.manpreet.flowdiary
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import flowdiary.composeapp.generated.resources.Res
-import flowdiary.composeapp.generated.resources.compose_multiplatform
+import fyi.manpreet.flowdiary.navigation.HomeDestination
+import fyi.manpreet.flowdiary.ui.home.HomeScreen
 import fyi.manpreet.flowdiary.ui.home.HomeViewModel
 import fyi.manpreet.flowdiary.ui.theme.FlowTheme
+import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
@@ -28,17 +19,16 @@ fun App(
     navController: NavHostController = rememberNavController(),
 ) {
     FlowTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
+
+        NavHost(
+            navController = navController,
+            startDestination = HomeDestination,
+        ) {
+
+            composable<HomeDestination> {
+                HomeScreen(
+                    navController = navController,
+                )
             }
         }
     }
