8345449f65fd6c78afc5ecfde3949c58c0c3c458
Manpreet Kunnath <manpreetkunnath@gmail.com>
2025-01-01 13:58:26 +0530

    Add splash screen

--- composeApp/src/androidMain/kotlin/fyi/manpreet/flowdiary/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/fyi/manpreet/flowdiary/MainActivity.kt
@@ -1,15 +1,26 @@
 package fyi.manpreet.flowdiary
 
 import android.os.Bundle
+import android.view.Window
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowInsetsControllerCompat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Remove the title bar
+        window.requestFeature(Window.FEATURE_NO_TITLE)
+        enableEdgeToEdge()
+        val insetsController = WindowInsetsControllerCompat(window, window.decorView)
+        insetsController.isAppearanceLightStatusBars = false
+
+        installSplashScreen()
         setContent {
             App()
         }
