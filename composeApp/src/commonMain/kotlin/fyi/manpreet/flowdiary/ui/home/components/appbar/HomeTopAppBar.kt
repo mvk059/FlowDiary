201f72f1c91a9efcb765bcc39f99ad2b19a5ba31
Manpreet Kunnath <manpreetkunnath@gmail.com>
2025-01-01 16:57:56 +0530

    Update App top bar text style

--- composeApp/src/commonMain/kotlin/fyi/manpreet/flowdiary/ui/home/components/appbar/HomeTopAppBar.kt
+++ composeApp/src/commonMain/kotlin/fyi/manpreet/flowdiary/ui/home/components/appbar/HomeTopAppBar.kt
@@ -5,6 +5,7 @@ import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
@@ -24,7 +25,12 @@ fun HomeTopAppBar(
 ) {
 
     TopAppBar(
-        title = { Text(text = stringResource(Res.string.home_appbar_title)) },
+        title = {
+            Text(
+                text = stringResource(Res.string.home_appbar_title),
+                style = MaterialTheme.typography.headlineLarge,
+            )
+        },
         actions = {
             IconButton(
                 onClick = { },
