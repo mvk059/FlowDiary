b110586d290202e24d0b76f70f782665061c0b29
Manpreet Kunnath <manpreetkunnath@gmail.com>
2025-01-02 12:10:36 +0530

    Extract ExpandableText into a separate file

---
+++ composeApp/src/commonMain/kotlin/fyi/manpreet/flowdiary/ui/home/components/list/ExpandableText.kt
@@ -0,0 +1,88 @@
+package fyi.manpreet.flowdiary.ui.home.components.list
+
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import flowdiary.composeapp.generated.resources.Res
+import flowdiary.composeapp.generated.resources.list_item_show_less
+import flowdiary.composeapp.generated.resources.list_item_show_more
+import fyi.manpreet.flowdiary.util.noRippleClickable
+import org.jetbrains.compose.resources.stringResource
+
+@Composable
+fun ExpandableText(
+    text: String,
+    modifier: Modifier = Modifier,
+    textModifier: Modifier = Modifier,
+    style: TextStyle = MaterialTheme.typography.bodyMedium,
+    collapsedMaxLine: Int = 3,
+    showMoreText: String = stringResource(Res.string.list_item_show_more),
+    showMoreStyle: SpanStyle = SpanStyle(
+        color = MaterialTheme.colorScheme.primary,
+        fontWeight = FontWeight.W500
+    ),
+    showLessText: String = stringResource(Res.string.list_item_show_less),
+    showLessStyle: SpanStyle = showMoreStyle,
+) {
+
+    var isExpanded by remember { mutableStateOf(false) }
+    var clickable by remember { mutableStateOf(false) }
+    var lastCharIndex by remember { mutableStateOf(0) }
+
+    Box(
+        modifier = Modifier
+            .noRippleClickable {
+                if (clickable) isExpanded = !isExpanded
+            }
+            .then(modifier)
+    ) {
+
+        Text(
+            modifier = textModifier
+                .fillMaxWidth()
+                .animateContentSize(),
+            text = buildAnnotatedString {
+                if (clickable) {
+                    if (isExpanded) {
+                        // Display the full text and "Show Less" button when expanded.
+                        append(text)
+                        withStyle(style = showLessStyle) { append(showLessText) }
+                    } else {
+                        // Display truncated text and "Show More" button when collapsed.
+                        val adjustText = text.substring(startIndex = 0, endIndex = lastCharIndex)
+                            .dropLast(showMoreText.length)
+                            .dropLastWhile { it == ' ' || it == '.' }
+                        append(adjustText)
+                        withStyle(style = showMoreStyle) { append(showMoreText) }
+                    }
+                } else {
+                    // Display the full text when not clickable.
+                    append(text)
+                }
+            },
+            maxLines = if (isExpanded) Int.MAX_VALUE else collapsedMaxLine,
+            // Determine visual overflow and enable click ability.
+            onTextLayout = { textLayoutResult ->
+                if (!isExpanded && textLayoutResult.hasVisualOverflow) {
+                    clickable = true
+                    lastCharIndex = textLayoutResult.getLineEnd(collapsedMaxLine - 1)
+                }
+            },
+            style = style,
+            color = MaterialTheme.colorScheme.onSurfaceVariant
+        )
+    }
+}
