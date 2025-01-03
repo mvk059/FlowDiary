c121010ef123771a25751905179cbe1e7a1e0179
Manpreet Kunnath <manpreetkunnath@gmail.com>
2025-01-03 10:00:39 +0530

    Link Settings screen

--- composeApp/src/commonMain/kotlin/fyi/manpreet/flowdiary/App.kt
+++ composeApp/src/commonMain/kotlin/fyi/manpreet/flowdiary/App.kt
@@ -7,8 +7,10 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import fyi.manpreet.flowdiary.navigation.HomeDestination
 import fyi.manpreet.flowdiary.navigation.NewRecordDestination
+import fyi.manpreet.flowdiary.navigation.SettingsDestination
 import fyi.manpreet.flowdiary.ui.home.HomeScreen
 import fyi.manpreet.flowdiary.ui.newrecord.NewRecordScreen
+import fyi.manpreet.flowdiary.ui.settings.SettingsScreen
 import fyi.manpreet.flowdiary.ui.theme.FlowTheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -29,12 +31,22 @@ fun App(
                     navController = navController,
                     onNewRecordClick = {
                         navController.navigate(NewRecordDestination)
+                    },
+                    onSettingsClick = {
+                        navController.navigate(SettingsDestination)
                     }
                 )
             }
 
             composable<NewRecordDestination> {
                 NewRecordScreen(
+                    navController = navController,
+                    onBackClick = { navController.navigateUp() }
+                )
+            }
+
+            composable<SettingsDestination> {
+                SettingsScreen(
                     navController = navController,
                     onBackClick = { navController.navigateUp() }
                 )
