b82ee077ce1b5d86139ecda2337d96e9a99572ec
Manpreet Kunnath <manpreetkunnath@gmail.com>
2025-01-03 21:47:12 +0530

    Update ExpandableText to click only on Show more

--- composeApp/src/commonMain/kotlin/fyi/manpreet/flowdiary/ui/home/components/list/ExpandableText.kt
+++ composeApp/src/commonMain/kotlin/fyi/manpreet/flowdiary/ui/home/components/list/ExpandableText.kt
@@ -11,15 +11,16 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.LinkAnnotation
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withLink
 import androidx.compose.ui.text.withStyle
 import flowdiary.composeapp.generated.resources.Res
 import flowdiary.composeapp.generated.resources.list_item_show_less
 import flowdiary.composeapp.generated.resources.list_item_show_more
-import fyi.manpreet.flowdiary.util.noRippleClickable
 import org.jetbrains.compose.resources.stringResource
 
 @Composable
@@ -42,37 +43,49 @@ fun ExpandableText(
     var clickable by remember { mutableStateOf(false) }
     var lastCharIndex by remember { mutableStateOf(0) }
 
-    Box(
-        modifier = Modifier
-            .noRippleClickable {
-                if (clickable) isExpanded = !isExpanded
+    val annotatedText = buildAnnotatedString {
+        if (clickable) {
+            if (isExpanded) {
+                // Display the full text and "Show Less" button when expanded.
+                append(text)
+                withLink(
+                    link = LinkAnnotation.Clickable(
+                        tag = "Show Less",
+                        linkInteractionListener = { isExpanded = !isExpanded }
+                    )
+                ) {
+                    withStyle(style = showLessStyle) { append(showLessText) }
+                }
+            } else {
+                // Display truncated text and "Show More" button when collapsed.
+                val adjustText = text.substring(startIndex = 0, endIndex = lastCharIndex)
+                    .dropLast(showMoreText.length)
+                    .dropLastWhile { it.isWhitespace() || it == '.' }
+                append(adjustText)
+                withLink(
+                    link = LinkAnnotation.Clickable(
+                        tag = "Show More",
+                        linkInteractionListener = { isExpanded = !isExpanded }
+                    )
+                ) {
+                    withStyle(style = showMoreStyle) { append(showMoreText) }
+                }
             }
-            .then(modifier)
+        } else {
+            // Display the full text when not clickable.
+            append(text)
+        }
+    }
+
+    Box(
+        modifier = modifier
     ) {
 
         Text(
             modifier = textModifier
                 .fillMaxWidth()
                 .animateContentSize(),
-            text = buildAnnotatedString {
-                if (clickable) {
-                    if (isExpanded) {
-                        // Display the full text and "Show Less" button when expanded.
-                        append(text)
-                        withStyle(style = showLessStyle) { append(showLessText) }
-                    } else {
-                        // Display truncated text and "Show More" button when collapsed.
-                        val adjustText = text.substring(startIndex = 0, endIndex = lastCharIndex)
-                            .dropLast(showMoreText.length)
-                            .dropLastWhile { it == ' ' || it == '.' }
-                        append(adjustText)
-                        withStyle(style = showMoreStyle) { append(showMoreText) }
-                    }
-                } else {
-                    // Display the full text when not clickable.
-                    append(text)
-                }
-            },
+            text = annotatedText,
             maxLines = if (isExpanded) Int.MAX_VALUE else collapsedMaxLine,
             // Determine visual overflow and enable click ability.
             onTextLayout = { textLayoutResult ->
