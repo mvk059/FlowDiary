793614461da0457aa91ed2d1f17742a89b8f548c
Manpreet Kunnath <manpreetkunnath@gmail.com>
2025-01-07 17:23:56 +0530

    Update colors

--- composeApp/src/commonMain/kotlin/fyi/manpreet/flowdiary/ui/theme/Colors.kt
+++ composeApp/src/commonMain/kotlin/fyi/manpreet/flowdiary/ui/theme/Colors.kt
@@ -37,10 +37,29 @@ val Secondary90 = Color(0xFFD9E2FF)
 val Secondary95 = Color(0xFFEEF0FF)
 val FabShadow = Color(0x4D1B6EF3)
 
+val Excited25 = Color(0xFF723602)
+val Excited35 = Color(0xFFDB6C0B)
+val Excited80 = Color(0xFFDDD2C8)
 val Excited95 = Color(0xFFF5F2EF)
+
+val Peaceful25 = Color(0xFF6C044D)
+val Peaceful35 = Color(0xFFBE3294)
+val Peaceful80 = Color(0xFFE1CEDB)
 val Peaceful95 = Color(0xFFF6F2F5)
+
+val Neutral25 = Color(0xFF0A5F33)
+val Neutral35 = Color(0xFF41B278)
+val Neutral80 = Color(0xFFB9DDCB)
 val Neutral95 = Color(0xFFEEF7F3)
+
+val Sad25 = Color(0xFF004585)
+val Sad35 = Color(0xFF3A8EDE)
+val Sad80 = Color(0xFFC5D8E9)
 val Sad95 = Color(0xFFEFF4F8)
+
+val Stressed25 = Color(0xFF6B0303)
+val Stressed35 = Color(0xFFDE3A3A)
+val Stressed80 = Color(0xFFE9C5C5)
 val Stressed95 = Color(0xFFF8EFEF)
 
 internal val ColorScheme = lightColorScheme(
