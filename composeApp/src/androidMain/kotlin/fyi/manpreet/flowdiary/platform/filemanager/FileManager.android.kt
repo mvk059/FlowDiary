897d80e1ece6ff822d340b7a7be45dbc86299dc5
Manpreet Kunnath <manpreetkunnath@gmail.com>
2025-01-10 07:12:09 +0530

    Implement reading amplitude data from file in android

--- composeApp/src/androidMain/kotlin/fyi/manpreet/flowdiary/platform/filemanager/FileManager.android.kt
+++ composeApp/src/androidMain/kotlin/fyi/manpreet/flowdiary/platform/filemanager/FileManager.android.kt
@@ -1,7 +1,12 @@
 package fyi.manpreet.flowdiary.platform.filemanager
 
 import arrow.core.raise.Raise
+import arrow.core.raise.catch
+import arrow.core.raise.ensure
+import fyi.manpreet.flowdiary.data.model.AmplitudeData
 import fyi.manpreet.flowdiary.usecase.MainActivityUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.io.File
 
 actual class FileManager(
@@ -13,6 +18,28 @@ actual class FileManager(
             File(filesDir, relativePath).absolutePath
         } catch (e: Exception) {
             raise("Failed to get file path: ${e.message}")
+        }
+    }
+
+    actual suspend fun Raise<String>.getAmplitudeData(path: String): List<AmplitudeData> {
+        return withContext(Dispatchers.IO) {
+            catch(
+                catch = { raise(it.message ?: "Unknown error loading amplitude data") },
+                block = {
+                    val amplitudesFile = File(path)
+                    ensure(amplitudesFile.exists()) { "File not found" }
+
+                    amplitudesFile.readLines()
+                        .flatMap { line ->
+                            line.splitToSequence(",")
+                        }
+                        .mapNotNull {
+                            if (it.isNotEmpty())
+                                AmplitudeData(it.toFloat())
+                            else null
+                        }
+                }
+            )
         }
     }
 }
--- composeApp/src/commonMain/kotlin/fyi/manpreet/flowdiary/platform/filemanager/FileManager.kt
+++ composeApp/src/commonMain/kotlin/fyi/manpreet/flowdiary/platform/filemanager/FileManager.kt
@@ -1,7 +1,9 @@
 package fyi.manpreet.flowdiary.platform.filemanager
 
 import arrow.core.raise.Raise
+import fyi.manpreet.flowdiary.data.model.AmplitudeData
 
 expect class FileManager {
     suspend fun Raise<String>.getFullImagePath(relativePath: String): String
+    suspend fun Raise<String>.getAmplitudeData(path: String): List<AmplitudeData>
 }
